21bd3abc42073db65acb0f66c4bbe300833b8989
lusir <1548936488@qq.com>
2018-05-24 16:56:32 +0800

    用户模块 完成

---
+++ src/main/java/com/lusirmall/common/Const.java
@@ -0,0 +1,16 @@
+package com.lusirmall.common;
+
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    //内部接口类对常量进行分组
+    public interface Role {
+        //用户
+        int ROLE_CUSTOMER = 0;
+        //管理员
+        int ROLE_ADMIN = 1;
+    }
+}
